cc9ea74110b52cef2e31a83629b8768fb075fbcf
Ashiq Sulaiman <Ashiq@Ashiqs-MacBook-Pro.local>
2018-02-26 12:28:08 +0530

    written unit and UITest cases

--- NestawayUITests/NestawayUITests.swift
+++ NestawayUITests/NestawayUITests.swift
@@ -9,19 +9,19 @@
 import XCTest
 
 class NestawayUITests: XCTestCase {
-        
+      var app: XCUIApplication!
+    
+    
+    
     override func setUp() {
         super.setUp()
-        
-        var app: XCUIApplication?
-        
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         app = XCUIApplication()
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launchArguments.append("--uitesting")
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -31,16 +31,41 @@ class NestawayUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+
     
-    
-    
-    func testSearchFunction(){
-       // app.launch()
+
+    func testSearchBar(){
+        app.launch()
+        let searchBar = app.otherElements["searchBar"]
+        searchBar.tap()
+        searchBar.searchFields.element.typeText("Avengers Infinity War")
+        app.buttons["Go"].tap()
+        XCTAssertEqual(app.keyboards.count, 0)
         
     }
+    
+    
+    func testScrollAndTapOnTweetTableView(){
+        app.launch()
+        let tablesQuery = app.tables
+        let tableElement = tablesQuery.element
+        tableElement.swipeUp()
+        let cellQuery = self.app.tables.cells.element(boundBy: 3)
+        cellQuery.tap()
+        XCTAssertTrue(app.isUserInfoScreen)
+    }
+    
+    
+    func testTappedOnBackFromUserInfoView(){
+        app.launch()
+        let cellQuery = self.app.tables.cells.element(boundBy: 3)
+        cellQuery.tap()
+        XCTAssertTrue(app.isUserInfoScreen)
+        app.navigationBars["Nestaway.UserInfoView"].buttons["Search Tweets"].tap()
+        XCTAssertTrue(app.isDisplayingSearchView)
+        
+    }
+    
+    
     
 }
